90a2a6fbda354700cf3672755e31a33c88ab787f
Bigfoot71 <victor.lejuez@protonmail.ch>
2024-04-19 00:59:23 +0200

    reviewed examples using X11

--- examples/X11/X11_3D.c
+++ examples/X11/X11_3D.c
@@ -1,129 +1,28 @@
-#include "pixelforge.h"
-#include <X11/Xlib.h>
-#include <sys/time.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#define PF_COMMON_IMPL
-#include "../common.h"
+#define PF_X11_COMMON_IMPL
+#include "X11_common.h"
 
 #define SCREEN_WIDTH    800
 #define SCREEN_HEIGHT   600
 
-/* Helper functions */
-
-unsigned long micros()
-{
-    struct timeval currentTime;
-    gettimeofday(&currentTime, NULL);
-    return currentTime.tv_sec * 1000000 + currentTime.tv_usec;
-}
-
-/* X11 management */
-
-typedef struct {
-    Display *dpy;
-    Window root;
-    Window win;
-    GC gc;
-    XEvent e;
-    Atom wmDeleteMessage;
-    int screen;
-} X11_App;
-
-X11_App X11_InitApp(int width, int height)
-{
-    X11_App app;
-
-    // Open a connection with the X server
-    app.dpy = XOpenDisplay(NULL);
-    if (!app.dpy) exit(-1);
-
-    // Get screen number and window root
-    app.screen = DefaultScreen(app.dpy);
-    app.root = RootWindow(app.dpy, app.screen);
-
-    // Create a simple window
-    app.win = XCreateSimpleWindow(app.dpy, app.root, 0, 0, width, height,
-        0, BlackPixel(app.dpy, app.screen), WhitePixel(app.dpy, app.screen));
-
-    // Select events to monitor
-    XSelectInput(app.dpy, app.win, ExposureMask | KeyPressMask | StructureNotifyMask);
-
-    // Create a graphic context
-    app.gc = XCreateGC(app.dpy, app.win, 0, NULL);
-
-    // Show window
-    XMapWindow(app.dpy, app.win);
-
-    // Get Window Removal Message
-    app.wmDeleteMessage = XInternAtom(app.dpy, "WM_DELETE_WINDOW", False);
-
-    // Set Window Suppression Protocol
-    XSetWMProtocols(app.dpy, app.win, &app.wmDeleteMessage, 1);
-
-    return app;
-}
-
-void X11_CloseApp(X11_App* app)
-{
-    XDestroyWindow(app->dpy, app->win);
-    XCloseDisplay(app->dpy);
-}
-
-/* Custom pixel setter/setter functions */
-
-void SetScreenPixel(void* pixels, PFsizei offset, PFcolor color)
-{
-    ((PFubyte*)pixels)[offset*4] = color.b;
-    ((PFubyte*)pixels)[offset*4 + 1] = color.g;
-    ((PFubyte*)pixels)[offset*4 + 2] = color.r;
-    ((PFubyte*)pixels)[offset*4 + 3] = color.a;
-}
-
-PFcolor GetScreenPixel(const void* pixels, PFsizei offset)
-{
-    return (PFcolor) {
-        ((PFubyte*)pixels)[offset*4 + 2],
-        ((PFubyte*)pixels)[offset*4 + 1],
-        ((PFubyte*)pixels)[offset*4],
-        ((PFubyte*)pixels)[offset*4 + 3]
-    };
-}
-
-/* Main */
-
-int main()
+int main(void)
 {
     // Init X11 application
     X11_App app = X11_InitApp(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    // Init buffer and XImage destination for PixelForge
-    PFuint *pixels = (PFuint*)malloc(SCREEN_WIDTH*SCREEN_HEIGHT*sizeof(PFuint));
-    XImage *image = XCreateImage(app.dpy, DefaultVisual(app.dpy, app.screen), DefaultDepth(app.dpy, app.screen),
-                                    ZPixmap, 0, (char*)pixels, SCREEN_WIDTH, SCREEN_HEIGHT, 32, 0);
-
     // Allocate destination buffer and init PixelForge context
-    PFctx *ctx = PF_Init(pixels, SCREEN_WIDTH, SCREEN_HEIGHT);
-    PF_Reshape(SCREEN_WIDTH, SCREEN_HEIGHT);
+    PFctx *ctx = PF_InitFromX11App(&app);
 
-    // Defining our own pixel getter/setter functions
-    pfSetDefaultPixelGetter(GetScreenPixel);
-    pfSetDefaultPixelSetter(SetScreenPixel);
-
-    // Set a timer for continuous rendering
-    const int frameDelay = 1000000 / 60; // 60 FPS
-    unsigned long frameStart;
-    int frameTime;
+    // Init clock system
+    Clock clock = Clock_Create(60);
 
     // Set camera position and phase for rotation
     PFMvec3 camPos = { -2.0f, 1.5f, -2.0f };
     float timer = 0;
 
+    // Main loop
     while (1)
     {
-        // Start of frame timer
-        frameStart = micros();
+        Clock_Begin(&clock);
 
         // Check X11 window events
         int windowClosed = 0;
@@ -150,30 +49,18 @@ int main()
 
         // Draw something on each iteration of the main loop
         PF_Begin3D(SCREEN_WIDTH, SCREEN_HEIGHT, 60.0);
-        PF_Update3D(camPos[0], camPos[1], camPos[2], 0, 0, 0);
-        PF_DrawCube(1);
+            PF_Update3D(camPos[0], camPos[1], camPos[2], 0, 0, 0);
+            PF_DrawCube(1);
         PF_End3D();
 
         // Draw the contents of the pixel buffer on the window
-        XPutImage(app.dpy, app.win, app.gc, image, 0, 0, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+        X11_UpdateWindow(&app);
 
-        // Calculate elapsed time during frame
-        // And wait until the end of the frameDelay
-        frameTime = micros() - frameStart;
-        if (frameDelay > frameTime)
-        {
-            usleep(frameDelay - frameTime);
-        }
+        Clock_End(&clock);
     }
 
-    // Destroy PixelForge context
+    // Freeing resources and closing X11_App
     pfDeleteContext(ctx);
-
-    // Free image and pixel buffer
-    XFree(image);
-    free(pixels);
-
-    // Close connection
     X11_CloseApp(&app);
 
     return 0;
---
+++ examples/X11/X11_common.h
@@ -0,0 +1,153 @@
+#ifndef PF_X11_COMMON_H
+#define PF_X11_COMMON_H
+
+#ifdef PF_X11_COMMON_H
+#   define PF_COMMON_IMPL
+#endif //PF_X11_COMMON_H
+
+#include "../common.h"
+#include <X11/Xlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/* Pixel getter/setter */
+
+PFcolor PF_GetPixel(const void* pixels, PFsizei offset)
+{
+    return (PFcolor) {
+        ((PFubyte*)pixels)[offset*4 + 2],
+        ((PFubyte*)pixels)[offset*4 + 1],
+        ((PFubyte*)pixels)[offset*4],
+        ((PFubyte*)pixels)[offset*4 + 3]
+    };
+}
+
+void PF_SetPixel(void* pixels, PFsizei offset, PFcolor color)
+{
+    ((PFubyte*)pixels)[offset*4] = color.b;
+    ((PFubyte*)pixels)[offset*4 + 1] = color.g;
+    ((PFubyte*)pixels)[offset*4 + 2] = color.r;
+    ((PFubyte*)pixels)[offset*4 + 3] = color.a;
+}
+
+/* X11 App management */
+
+typedef struct {
+    Display *dpy;
+    Window root;
+    Window win;
+    GC gc;
+    XEvent e;
+    Atom wmDeleteMessage;
+    int screen;
+    PFuint *destBuffer;
+    XImage *destImage;
+} X11_App;
+
+X11_App X11_InitApp(int width, int height)
+{
+    X11_App app;
+
+    // Open a connection with the X server
+    app.dpy = XOpenDisplay(NULL);
+    if (!app.dpy) exit(1);
+
+    // Get screen number and window root
+    app.screen = DefaultScreen(app.dpy);
+    app.root = RootWindow(app.dpy, app.screen);
+
+    // Create a simple window
+    app.win = XCreateSimpleWindow(app.dpy, app.root, 0, 0, width, height,
+        0, BlackPixel(app.dpy, app.screen), WhitePixel(app.dpy, app.screen));
+
+    // Select events to monitor
+    XSelectInput(app.dpy, app.win, ExposureMask | KeyPressMask | StructureNotifyMask);
+
+    // Create a graphic context
+    app.gc = XCreateGC(app.dpy, app.win, 0, NULL);
+
+    // Show window
+    XMapWindow(app.dpy, app.win);
+
+    // Get Window Removal Message
+    app.wmDeleteMessage = XInternAtom(app.dpy, "WM_DELETE_WINDOW", False);
+
+    // Set Window Suppression Protocol
+    XSetWMProtocols(app.dpy, app.win, &app.wmDeleteMessage, 1);
+
+    // Create destination buffer
+    app.destBuffer = (PFuint*)malloc(width*height*sizeof(PFuint));
+    app.destImage = XCreateImage(app.dpy, DefaultVisual(app.dpy, app.screen), DefaultDepth(app.dpy, app.screen),
+                                    ZPixmap, 0, (char*)app.destBuffer, width, height, 32, 0);
+
+    return app;
+}
+
+void X11_CloseApp(X11_App* app)
+{
+    XFree(app->destImage);
+    free(app->destBuffer);
+
+    XDestroyWindow(app->dpy, app->win);
+    XCloseDisplay(app->dpy);
+}
+
+void X11_UpdateWindow(X11_App* app)
+{
+    XPutImage(app->dpy, app->win, app->gc, app->destImage,
+        0, 0, 0, 0, app->destImage->width, app->destImage->height);
+}
+
+/* Clock management */
+
+typedef struct {
+    struct timeval tvLastFrame;
+    float deltaTime;
+    unsigned int maxFPS;
+} Clock;
+
+Clock Clock_Create(unsigned int maxFPS)
+{
+    Clock clock;
+    gettimeofday(&clock.tvLastFrame, NULL);
+    clock.deltaTime = 0.0f;
+    clock.maxFPS = maxFPS;
+    return clock;
+}
+
+void Clock_Begin(Clock* clock)
+{
+    gettimeofday(&clock->tvLastFrame, NULL);
+}
+
+void Clock_End(Clock* clock)
+{
+    struct timeval tvThisFrame;
+    gettimeofday(&tvThisFrame, NULL);
+    unsigned int ticksAtThisFrame = (tvThisFrame.tv_sec * 1000) + (tvThisFrame.tv_usec / 1000);
+    unsigned int ticksSinceLastFrame = (ticksAtThisFrame - (clock->tvLastFrame.tv_sec * 1000) - (clock->tvLastFrame.tv_usec / 1000));
+    float targetDeltaTime = 1000.0f / clock->maxFPS;
+
+    if (ticksSinceLastFrame < targetDeltaTime)
+    {
+        unsigned int delayMilliseconds = (unsigned int)(targetDeltaTime - ticksSinceLastFrame);
+        usleep(delayMilliseconds * 1000);
+    }
+
+    gettimeofday(&clock->tvLastFrame, NULL);
+    unsigned int ticksAtLastFrame = (clock->tvLastFrame.tv_sec * 1000) + (clock->tvLastFrame.tv_usec / 1000);
+    clock->deltaTime = (float)(ticksAtLastFrame - ticksAtThisFrame) / 1000.0f;
+}
+
+/* PixelForge context management */
+
+PFctx* PF_InitFromX11App(X11_App* app)
+{
+    PFctx *ctx = PF_Init(app->destBuffer, app->destImage->width, app->destImage->height);
+    pfSetDefaultPixelGetter(PF_GetPixel);
+    pfSetDefaultPixelSetter(PF_SetPixel);
+    return ctx;
+}
+
+#endif //PF_X11_COMMON_H
