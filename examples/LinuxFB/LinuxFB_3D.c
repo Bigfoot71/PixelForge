dc78f675ddcbec8e7ce9d3248710447ffbd04524
Bigfoot71 <victor.lejuez@protonmail.ch>
2024-05-03 22:32:56 +0200

    add example using `/dev/fb0` directly on Linux

---
+++ examples/LinuxFB/LinuxFB_3D.c
@@ -0,0 +1,130 @@
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <math.h>
+
+#define PF_COMMON_IMPL
+#include "../common.h"
+
+static void PixelSetter(void *framebuffer, PFsizei index, PFcolor color)
+{
+    ((uint32_t*)framebuffer)[index] = (color.r << 16) | (color.g << 8) | color.b;
+}
+
+static PFcolor PixelGetter(const void *framebuffer, PFsizei index)
+{
+    uint32_t pixel = ((const uint32_t *)framebuffer)[index];
+
+    return (PFcolor) {
+        .r = (pixel >> 16) & 0xFF,
+        .g = (pixel >> 8) & 0xFF,
+        .b = pixel & 0xFF,
+        .a = 0xFF
+    };
+}
+
+int main(void)
+{
+    int fbFd = open("/dev/fb0", O_RDWR);
+    if (fbFd == -1)
+    {
+        perror("Error opening framebuffer device");
+        exit(1);
+    }
+
+    struct fb_var_screeninfo vinfo;
+    if (ioctl(fbFd, FBIOGET_VSCREENINFO, &vinfo))
+    {
+        perror("Error reading variable information");
+        close(fbFd);
+        exit(1);
+    }
+
+    size_t screensize = vinfo.yres_virtual * vinfo.xres_virtual * vinfo.bits_per_pixel / 8;
+    char* fbMem = (char*)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fbFd, 0);
+    if ((intptr_t)fbMem == -1)
+    {
+        perror("Error mapping framebuffer device to memory");
+        close(fbFd);
+        exit(1);
+    }
+
+    // Allocate two frame buffers
+    char* frontBuffer = (char*)malloc(screensize);
+    if (frontBuffer == NULL)
+    {
+        perror("Error allocating front buffer");
+        munmap(fbMem, screensize);
+        close(fbFd);
+        exit(1);
+    }
+
+    char* backBuffer = (char*)malloc(screensize);
+    if (backBuffer == NULL)
+    {
+        perror("Error allocating back buffer");
+        free(frontBuffer);
+        munmap(fbMem, screensize);
+        close(fbFd);
+        exit(1);
+    }
+
+    // Initialize frame buffers with framebuffer data
+    memcpy(frontBuffer, fbMem, screensize);
+    memcpy(backBuffer, fbMem, screensize);
+
+    // Creating the PixelForge rendering context
+    PFctx *ctx = pfCreateContext(backBuffer, vinfo.xres_virtual, vinfo.yres_virtual, PF_PIXELFORMAT_R8G8B8);
+    pfMakeCurrent(ctx);
+
+    // Definition of custom pixel get/set function
+    pfSetDefaultPixelGetter(PixelGetter);
+    pfSetDefaultPixelSetter(PixelSetter);
+
+    // Set the backBuffer as auxiliary buffer
+    pfSetAuxBuffer(frontBuffer);
+
+    // Define the camera position and a phase for the rotation
+    PFMvec3 camPos = { -2.0f, 1.5f, -2.0f };
+    float timer = 0;
+
+    while (1)
+    {
+        // Update camera position
+        camPos[0] = 2.0f * cosf(timer);
+        camPos[2] = 2.0f * sinf(timer);
+        timer += 2.0f * 0.032f;
+
+        // Clear the destination buffer (frontBuffer)
+        pfClear(PF_COLOR_BUFFER_BIT | PF_DEPTH_BUFFER_BIT);
+
+        // Draw something on each iteration of the main loop
+        PF_Begin3D(vinfo.xres_virtual, vinfo.yres_virtual, 60.0);
+            PF_Update3D(camPos[0], camPos[1], camPos[2], 0, 0, 0);
+            PF_DrawCube(1.0f);
+        PF_End3D();
+
+        // Swap buffers (frontBuffer becomes backBuffer and vice versa)
+        pfSwapBuffers();
+
+        // Copy the content of backBuffer into the framebuffer
+        memcpy(fbMem, backBuffer, screensize);
+
+        // Wait for a short while (about 16 ms)
+        usleep(16000);
+    }
+
+    // Free allocated memory
+    free(frontBuffer);
+    free(backBuffer);
+
+    // Close framebuffer
+    munmap(fbMem, screensize);
+    close(fbFd);
+
+    return 0;
+}
---
+++ examples/LinuxFB/LinuxFB_Gears.c
@@ -0,0 +1,376 @@
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <linux/input.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <math.h>
+
+#define PF_COMMON_IMPL
+#include "../common.h"
+
+static void PixelSetter(void *framebuffer, PFsizei index, PFcolor color)
+{
+    ((uint32_t*)framebuffer)[index] = (color.r << 16) | (color.g << 8) | color.b;
+}
+
+static PFcolor PixelGetter(const void *framebuffer, PFsizei index)
+{
+    uint32_t pixel = ((const uint32_t *)framebuffer)[index];
+
+    return (PFcolor) {
+        .r = (pixel >> 16) & 0xFF,
+        .g = (pixel >> 8) & 0xFF,
+        .b = pixel & 0xFF,
+        .a = 0xFF
+    };
+}
+
+static void Gear_Draw(float innerRadius, float outerRadius, float width, int teeth, float toothDepth)
+{
+    int i;
+    float r0, r1, r2;
+    float angle, da;
+    float u, v, len;
+
+    r0 = innerRadius;
+    r1 = outerRadius - toothDepth/2.0f;
+    r2 = outerRadius + toothDepth/2.0f;
+
+    da = 2.0f*M_PI / teeth / 4.0f;
+
+    pfShadeModel(PF_FLAT);
+
+    pfNormal3f(0.0f, 0.0f, 1.0f);
+
+    // Draw front face
+    pfBegin(PF_QUAD_STRIP);
+        for (i = 0; i <= teeth; i++)
+        {
+            angle = i * 2.0f*M_PI / teeth;
+            pfVertex3f(r0*cos(angle), r0*sin(angle), width*0.5f);
+            pfVertex3f(r1*cos(angle), r1*sin(angle), width*0.5f);
+            pfVertex3f(r0*cos(angle), r0*sin(angle), width*0.5f);
+            pfVertex3f(r1*cos(angle+3*da), r1*sin(angle+3*da), width*0.5f);
+        }
+    pfEnd();
+
+    // Draw front sides of teeth
+    pfBegin(PF_QUADS);
+        da = 2.0f*M_PI / teeth / 4.0f;
+        for (i = 0; i < teeth; i++)
+        {
+            angle = i * 2.0f*M_PI / teeth;
+
+            pfVertex3f(r1*cos(angle),      r1*sin(angle),      width*0.5f);
+            pfVertex3f(r2*cos(angle+da),   r2*sin(angle+da),   width*0.5f);
+            pfVertex3f(r2*cos(angle+2*da), r2*sin(angle+2*da), width*0.5f);
+            pfVertex3f(r1*cos(angle+3*da), r1*sin(angle+3*da), width*0.5f);
+        }
+    pfEnd();
+
+    pfNormal3f(0.0f, 0.0f, -1.0f);
+
+    // Draw back face
+    pfBegin(PF_QUAD_STRIP);
+        for (i = 0; i <= teeth; i++)
+        {
+            angle = i * 2.0f*M_PI / teeth;
+            pfVertex3f(r1*cos(angle), r1*sin(angle), -width*0.5f);
+            pfVertex3f(r0*cos(angle), r0*sin(angle), -width*0.5f);
+            pfVertex3f(r1*cos(angle+3*da), r1*sin(angle+3*da), -width*0.5f);
+            pfVertex3f(r0*cos(angle), r0*sin(angle), -width*0.5f);
+        }
+    pfEnd();
+
+    // Draw back sides of teeth
+    pfBegin(PF_QUADS);
+    da = 2.0f*M_PI / teeth / 4.0f;
+        for (i = 0; i < teeth; i++)
+        {
+            angle = i * 2.0f*M_PI / teeth;
+
+            pfVertex3f(r1*cos(angle+3*da), r1*sin(angle+3*da), -width*0.5f);
+            pfVertex3f(r2*cos(angle+2*da), r2*sin(angle+2*da), -width*0.5f);
+            pfVertex3f(r2*cos(angle+da),   r2*sin(angle+da),   -width*0.5f);
+            pfVertex3f(r1*cos(angle),      r1*sin(angle),      -width*0.5f);
+        }
+    pfEnd();
+
+    // Draw outward faces of teeth
+    pfBegin(PF_QUAD_STRIP);
+        for (i=0;i<teeth;i++)
+        {
+            angle = i * 2.0f*M_PI / teeth;
+
+            pfVertex3f(r1*cos(angle),      r1*sin(angle),       width*0.5f);
+            pfVertex3f(r1*cos(angle),      r1*sin(angle),      -width*0.5f);
+            u = r2*cos(angle+da) - r1*cos(angle);
+            v = r2*sin(angle+da) - r1*sin(angle);
+            len = sqrt(u*u + v*v);
+            u /= len;
+            v /= len;
+            pfNormal3f(v, -u, 0.0f);
+            pfVertex3f(r2*cos(angle+da),   r2*sin(angle+da),    width*0.5f);
+            pfVertex3f(r2*cos(angle+da),   r2*sin(angle+da),   -width*0.5f);
+            pfNormal3f(cos(angle), sin(angle), 0.0f);
+            pfVertex3f(r2*cos(angle+2*da), r2*sin(angle+2*da),  width*0.5f);
+            pfVertex3f(r2*cos(angle+2*da), r2*sin(angle+2*da), -width*0.5f);
+            u = r1*cos(angle+3*da) - r2*cos(angle+2*da);
+            v = r1*sin(angle+3*da) - r2*sin(angle+2*da);
+            pfNormal3f(v, -u, 0.0f);
+            pfVertex3f(r1*cos(angle+3*da), r1*sin(angle+3*da),  width*0.5f);
+            pfVertex3f(r1*cos(angle+3*da), r1*sin(angle+3*da), -width*0.5f);
+            pfNormal3f(cos(angle), sin(angle), 0.0f);
+        }
+
+        pfVertex3f(r1*cos(0), r1*sin(0), width*0.5f);
+        pfVertex3f(r1*cos(0), r1*sin(0), -width*0.5f);
+    pfEnd();
+
+    pfShadeModel(PF_SMOOTH);
+
+    // Draw inside radius cylinder
+    pfBegin(PF_QUAD_STRIP);
+        for (i = 0; i <= teeth; i++)
+        {
+            angle = i * 2.0f*M_PI / teeth;
+            pfNormal3f(-cos(angle), -sin(angle), 0.0f);
+            pfVertex3f(r0*cos(angle), r0*sin(angle), -width*0.5f);
+            pfVertex3f(r0*cos(angle), r0*sin(angle), width*0.5f);
+        }
+    pfEnd();
+}
+
+
+/* Program */
+
+static float viewRotX = 20.0f;
+static float viewRotY = 30.0f;
+static float viewRotZ = 0.0f;
+static float angle = 0.0f;
+
+void Gears_Init(void)
+{
+   PFMvec3 pos = { 5.0f, 5.0f, 10.0f };
+   PFMvec3 dir = { 0 };
+
+    pfmVec3Sub(dir, dir, pos);
+    pfmVec3Normalize(dir, dir);
+
+    pfLightfv(PF_LIGHT0, PF_POSITION, pos);
+    pfLightfv(PF_LIGHT0, PF_SPOT_DIRECTION, dir);
+    pfEnable(PF_CULL_FACE);
+    pfEnable(PF_LIGHTING);
+    pfEnableLight(PF_LIGHT0);
+    pfEnable(PF_DEPTH_TEST);
+}
+
+void Gears_Reshape(int width, int height)
+{
+    float aspectRatio = (float)height / width;
+
+    pfViewport(0, 0, width, height);
+    pfMatrixMode(PF_PROJECTION);
+    pfLoadIdentity();
+    pfFrustum(-1.0f, 1.0f, -aspectRatio, aspectRatio, 5.0f, 60.0f);
+    pfMatrixMode(PF_MODELVIEW);
+    pfLoadIdentity();
+    pfTranslatef(0.0f, 0.0f, -40.0f);
+}
+
+void Gears_Draw(void)
+{
+    pfClear(PF_COLOR_BUFFER_BIT | PF_DEPTH_BUFFER_BIT);
+
+    pfEnable(PF_COLOR_MATERIAL);
+    pfColorMaterial(PF_FRONT_AND_BACK, PF_AMBIENT_AND_DIFFUSE);
+
+    pfPushMatrix();
+
+        pfRotatef(viewRotX, 1.0f, 0.0f, 0.0f);
+        pfRotatef(viewRotY, 0.0f, 1.0f, 0.0f);
+        pfRotatef(viewRotZ, 0.0f, 0.0f, 1.0f);
+
+        pfPushMatrix();
+            pfTranslatef(-3.0f, -2.0f, 0.0f);
+            pfRotatef(angle, 0.0f, 0.0f, 1.0f);
+            pfColor3ub(255, 0, 0);
+            Gear_Draw(1.0f, 4.0f, 1.0f, 20, 0.7f);
+        pfPopMatrix();
+
+        pfPushMatrix();
+            pfTranslatef(3.1f, -2.0f, 0.0f);
+            pfRotatef(-2.0f*angle-9.0f, 0.0f, 0.0f, 1.0f);
+            pfColor3ub(0, 255, 0);
+            Gear_Draw(0.5f, 2.0f, 2.0f, 10, 0.7f);
+        pfPopMatrix();
+
+        pfPushMatrix();
+            pfTranslatef(-3.1f, 4.2, 0.0f);
+            pfRotatef(-2.0f*angle-25.0f, 0.0f, 0.0f, 1.0f);
+            pfColor3ub(0, 0, 255);
+            Gear_Draw(1.3f, 2.0f, 0.5f, 10, 0.7f);
+        pfPopMatrix();
+
+    pfPopMatrix();
+
+    pfDisable(PF_COLOR_MATERIAL);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <linux/input.h>
+
+int main(void)
+{
+    int fbFd = open("/dev/fb0", O_RDWR);
+    if (fbFd == -1)
+    {
+        perror("Error opening framebuffer device");
+        exit(1);
+    }
+
+    struct fb_var_screeninfo vinfo;
+    if (ioctl(fbFd, FBIOGET_VSCREENINFO, &vinfo))
+    {
+        perror("Error reading variable information");
+        close(fbFd);
+        exit(1);
+    }
+
+    size_t screensize = vinfo.yres_virtual * vinfo.xres_virtual * vinfo.bits_per_pixel / 8;
+    char* fbMem = (char*)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fbFd, 0);
+    if ((intptr_t)fbMem == -1)
+    {
+        perror("Error mapping framebuffer device to memory");
+        close(fbFd);
+        exit(1);
+    }
+
+    // Allocate two frame buffers
+    char* frontBuffer = (char*)malloc(screensize);
+    if (frontBuffer == NULL)
+    {
+        perror("Error allocating front buffer");
+        munmap(fbMem, screensize);
+        close(fbFd);
+        exit(1);
+    }
+
+    char* backBuffer = (char*)malloc(screensize);
+    if (backBuffer == NULL)
+    {
+        perror("Error allocating back buffer");
+        free(frontBuffer);
+        munmap(fbMem, screensize);
+        close(fbFd);
+        exit(1);
+    }
+
+    // Opening keyboard input device
+    // NOTE: event2 may change depending on your system
+    /*
+    int fd = open("/dev/input/event2", O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening keyboard device");
+        return 1;
+    }
+    */
+
+    // Initialize frame buffers with framebuffer data
+    memcpy(frontBuffer, fbMem, screensize);
+    memcpy(backBuffer, fbMem, screensize);
+
+    // Creating the PixelForge rendering context
+    PFctx *ctx = pfCreateContext(backBuffer, vinfo.xres_virtual, vinfo.yres_virtual, PF_PIXELFORMAT_R8G8B8);
+    pfMakeCurrent(ctx);
+
+    // Definition of custom pixel get/set function
+    pfSetDefaultPixelGetter(PixelGetter);
+    pfSetDefaultPixelSetter(PixelSetter);
+
+    // Set the backBuffer as auxiliary buffer
+    pfSetAuxBuffer(frontBuffer);
+
+    // Init and reshape
+    Gears_Init();
+    Gears_Reshape(
+        vinfo.xres_virtual,
+        vinfo.yres_virtual);
+
+    struct input_event ev;
+    int quit = 0;
+    while (!quit)
+    {
+        // Reading keyboard input
+        /*
+        read(fd, &ev, sizeof(struct input_event));
+        if (ev.type == EV_KEY && ev.value == 1)
+        {
+            switch (ev.code)
+            {
+                case KEY_UP:
+                    viewRotX += 5.0f;
+                    break;
+                case KEY_DOWN:
+                    viewRotX -= 5.0f;
+                    break;
+                case KEY_LEFT:
+                    viewRotY += 5.0f;
+                    break;
+                case KEY_RIGHT:
+                    viewRotY -= 5.0f;
+                    break;
+                case KEY_Z:
+                    if (ev.value == 1 && (ev.type == EV_KEY || ev.type == EV_SYN)) {
+                        viewRotZ += 5.0f;
+                        if (ev.code == KEY_LEFTSHIFT || ev.code == KEY_RIGHTSHIFT) {
+                            viewRotZ -= 10.0f;
+                        }
+                    }
+                    break;
+                case KEY_ESC:
+                    quit = 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+        */
+
+        // Update and draw
+        angle += 90 * 0.016f;
+        Gears_Draw();
+
+        // Swap buffers (frontBuffer becomes backBuffer and vice versa)
+        pfSwapBuffers();
+
+        // Copy the content of backBuffer into the framebuffer
+        memcpy(fbMem, backBuffer, screensize);
+
+        // Wait for a short while (about 16 ms)
+        usleep(16000);
+    }
+
+    // Free allocated memory
+    free(frontBuffer);
+    free(backBuffer);
+
+    // Closing keyboard input device
+    //close(fd);
+
+    // Close framebuffer
+    munmap(fbMem, screensize);
+    close(fbFd);
+
+    return 0;
+}
