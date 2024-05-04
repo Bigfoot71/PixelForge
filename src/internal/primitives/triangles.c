72839c2498dc07eaaf2f2e79a60b978db8c9c1e9
Bigfoot71 <victor.lejuez@protonmail.ch>
2024-05-05 00:40:38 +0200

    update file

--- src/internal/primitives/triangles.c
+++ src/internal/primitives/triangles.c
@@ -1,5 +1,5 @@
-#ifdef PF_BRESENHAM_RASTER_METHOD
-#   include "./triangles_bresenham.c"
+#ifdef PF_SCANLINES_METHOD
+#   include "./triangles_scanlines.c"
 #else //PF_BARYCENTRIC_RASTER_METHOD
 #   include "./triangles_barycentric.c"
 #endif //PF_RASTER_METHOD
