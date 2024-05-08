1b4d2b5dc36a3628f3dbe2d1fe62528c48eaf627
Bigfoot71 <victor.lejuez@protonmail.ch>
2024-05-09 00:21:53 +0200

    add the license to the files

--- src/internal/primitives/triangles/triangles.h
+++ src/internal/primitives/triangles/triangles.h
@@ -1,16 +1,27 @@
+/**
+ *  Copyright (c) 2024 Le Juez Victor
+ *
+ *  This software is provided "as-is", without any express or implied warranty. In no event 
+ *  will the authors be held liable for any damages arising from the use of this software.
+ *
+ *  Permission is granted to anyone to use this software for any purpose, including commercial 
+ *  applications, and to alter it and redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented; you must not claim that you 
+ *  wrote the original software. If you use this software in a product, an acknowledgment 
+ *  in the product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such, and must not be misrepresented
+ *  as being the original software.
+ *
+ *   3. This notice may not be removed or altered from any source distribution.
+ */
+
 #ifndef PF_TRIANGLES_H
 #define PF_TRIANGLES_H
 
 #include "../../context.h"
 #include "../../config.h"
-
-typedef enum {
-    RASTER_TEXTURE  = 0x01,
-    RASTER_DEPTH    = 0x02,
-    RASTER_LIGHT    = 0x04,
-    RASTER_FRONT    = 0x08,
-    RASTER_3D       = 0x10
-} TriangleRasterMode;
 
 PFboolean Process_ProjectAndClipTriangle(PFvertex* restrict polygon, int_fast8_t* restrict vertexCounter, const PFMmat4 mvp);
 void Rasterize_Triangle(PFface faceToRender, PFboolean is3D, const PFvertex* v1, const PFvertex* v2, const PFvertex* v3, const PFMvec3 viewPos);
